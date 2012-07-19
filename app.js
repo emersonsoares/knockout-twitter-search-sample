f6a58ce1c65bc4c96f46d06694f99abd5e7d7f40
Emerson Soares <dev.emerson@gmail.com>
2012-07-18 21:39:52 -0400

    twitter search

--- app.js
+++ app.js
@@ -1,31 +1,47 @@
-// Here's my data model
 jQuery(function($) {
 
-	var ViewModel = function() {
-    	var self = this;
+    var TwitterViewModel = function() {
+        var self = this;
 
-    	self.timeline = ko.observableArray();
+        self.query = ko.observable();
 
-		
-		self.updateStream = function() {
-			$.ajax({
-      			dataType: 'jsonp'
-      			,url: 'https://api.twitter.com/1/statuses/user_timeline.json'
-      			,data: { screen_name: 'emerson_soares', count: 1 }
-      			,success: function(json) {
-         			$.each(json, function(i, tweet) {
-         				console.log(tweet);
-         				self.timeline.push({ tweet : tweet });
-         			});
-      			}
-      			,error: function( jqXHR, textStatus, errorThrown ) {
-         			console.log( 'error: ' + textStatus);
-      			}
-      			,timeout:5000
-      			,retryMax: 2
-   			});
-		};
-	};
+        self.tweets = ko.observableArray();
+
+        self.currentPage = ko.observable(1);
+
+        self.searchTweets = function() {
+            self.tweets.removeAll();
+            self.getTweets(self.query);
+        };
+
+        self.getTweets = function(query, currentPage) {
+            $.ajax({
+                dataType: 'jsonp'
+                ,url: 'http://search.twitter.com/search.json'
+                ,data: { q: query, include_entities: true, page: currentPage, rpp: 10}
+                ,success: function(json) {
+                    $.each(json.results, function(i, current){
+                        self.tweets.push({ 
+                            tweet: current.text,
+                            user: current.from_user
+                        });
+                    });
+                    self.currentPage(json.page);
+                }
+                ,error: function( jqXHR, textStatus, errorThrown ) {
+                    console.log( 'error: ' + textStatus);
+                }
+                ,timeout:5000
+                ,retryMax: 2
+            });
+        };
+
+        $(window).scroll(function () {
+            if ($(this).height() + $(this).scrollTop() == $(document).height()) {
+                self.getTweets(self.query, self.currentPage() + 1);
+            }
+        });
+    };
  
-	ko.applyBindings(new ViewModel()); // This makes Knockout get to work​​​
+    ko.applyBindings(new TwitterViewModel());
 });
